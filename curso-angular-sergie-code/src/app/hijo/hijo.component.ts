0cc5c6ee64399754b53f6260f3ec6153633d2664
LuisPerezLec <luigiperez2002@gmail.com>
2024-03-26 22:09:00 -0600

    Componentes, Input/Output Padre <-> Hijo

---
+++ curso-angular-sergie-code/src/app/hijo/hijo.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.css']
+})
+export class HijoComponent {
+  //Este es el mensaje que recibe desde el padre
+  // @Input() recibeHijo: string = '';
+
+  @Output() mensajeDesdeHijo = new EventEmitter<string>();
+  mensajeHijoAPadre = ''
+
+  enviarMensaje(){
+    this.mensajeDesdeHijo.emit(this.mensajeHijoAPadre)
+  }
+
+}
